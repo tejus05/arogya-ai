f8b590fe41f01ac0aafa868d7d2bea7ac6fd9e50
tejus <thequantumcoder@gmail.com>
2024-05-17 23:58:13 +0530

    commit

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,9 +8,9 @@ const cardMap = [
   {
     title: "Yoga Guru",
     link: "",
-    buttonText: "Explore",
+    buttonText: "Start Practicing",
     description:
-      "Start analysing your yoga poses with our advanced AI-powered algorithms. ",
+      "Improve your yoga poses with our advanced AI-powered algorithms. Perfect your asanas and enhance your flexibility.",
     imageAlt: "",
     imageSrc:
       "https://images.shiksha.com/mediadata/images/articles/1689079450php3qZVMP.jpeg",
@@ -18,8 +18,9 @@ const cardMap = [
   {
     title: "Community",
     link: "community",
-    buttonText: "Explore",
-    description: "Explore the vibrant community of health freaks",
+    buttonText: "Join Now",
+    description:
+      "Become a part of our vibrant community of health enthusiasts. Share your journey, learn from others, and stay motivated.",
     imageAlt: "",
     imageSrc:
       "https://inspireyoga.com/wp-content/uploads/2021/02/InspireYogaGV-032019_PRT-25-scaled.jpg",
@@ -27,8 +28,9 @@ const cardMap = [
   {
     title: "Arogya Saathi",
     link: "",
-    buttonText: "Explore",
-    description: "",
+    buttonText: "Get Results",
+    description:
+      "Get personalized health advice from our Arogya Saathi. Make informed decisions about your health and wellness with our AI-powered solution.",
     imageAlt: "",
     imageSrc:
       "https://www.priviahealth.com/wp-content/uploads/2019/09/03_20_17_Why-the-Doctor-Patient-Relationship-is-Important-e1530037975292-1200x800.jpg",
@@ -36,16 +38,18 @@ const cardMap = [
   {
     title: "Progress Tracker",
     link: "progress-tracker",
-    buttonText: "Explore",
-    description: "",
+    buttonText: "Track Now",
+    description:
+      "Monitor your health progress with our intuitive tracker. Set goals, track them, and celebrate your achievements.",
     imageAlt: "",
     imageSrc: "",
   },
   {
     title: "MentCare",
     link: "",
-    buttonText: "Explore",
-    description: "",
+    buttonText: "Start Therapy",
+    description:
+      "Connect with our AI chatbot, MentCare, for instant mental health support. Get insights, tips, and resources tailored to your needs.",
     imageAlt: "",
     imageSrc:
       "https://assets-global.website-files.com/61cb94e5fee3d491ca9aa59c/6494b0deaae5793861ffecf6_61cb94e5fee3d458719aa9f8_first-therapy-appointment.png",
@@ -64,7 +68,7 @@ const Dashboard = () => {
               imgAlt=""
               imgSrc={card.imageSrc}
               link={card.link}
-              key={card.link}
+              key={card.buttonText}
             />
         ))
       }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -17,14 +17,15 @@ export const Card = ({
   buttonText: string,
   link: string,
 }) => {
+  console.log(buttonText);
   return (
     <div className="w-60 rounded-xl shadow-lg flex flex-col m-2 bg-white">
       { (
         <img src={imgSrc} alt={imgAlt} className="w-full rounded-t-xl" />
       )}
-      {title && <h1 className="m-2">{title}</h1>}
+      {title && <h1 className="m-2 font-medium text-lg">{title}</h1>}
       {description && <p className="m-2">{description}</p>}
-      {buttonText && link && (
+      { (
         <a
           href={link}
           className="text-center w-11/12 rounded-md py-2 bg-blue-700 text-white no-underline m-2"
